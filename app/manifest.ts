13f65cc507ef29037a15ddc5607e7e8fc275d0db
Shawn <a123@ShawndeMacBook-Air.local>
2026-07-17 22:55:54 +0800

    Update mobile app icon artwork

--- app/manifest.ts
+++ app/manifest.ts
@@ -10,9 +10,7 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: '#FAFAF8',
     theme_color: '#C9A96E',
     icons: [
-      { src: '/favicon.ico', sizes: 'any', type: 'image/x-icon' },
-      { src: '/favicon.ico', sizes: '192x192', type: 'image/x-icon' },
-      { src: '/favicon.ico', sizes: '512x512', type: 'image/x-icon' },
+      { src: '/icon.png', sizes: '1254x1254', type: 'image/png', purpose: 'any' },
     ],
   }
 }
